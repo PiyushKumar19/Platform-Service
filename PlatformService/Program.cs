4f6da104b3d6307f1b6ef0c925c64b2d9c190b46
Piyush <developerpiyush1102k2@gmail.com>
2023-12-15 19:01:30 +0530

    Added AutoMapper Platform profiles for create and read

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
